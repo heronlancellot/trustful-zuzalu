a8735859035b719c291582e9cae1b924797afbb0
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-01 20:51:28 -0300

    feat: add title in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { Providers } from "./providers";
 const open_sans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next-Web3-Boilerplate",
+  title: "Trustful",
   applicationName: "Next Web3 Boilerplate",
   description: "Next.js Web3 boilerplate built on Wagmi, Viem, and Rainbow",
   authors: {
